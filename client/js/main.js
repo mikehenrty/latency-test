266d9190e6f7ed2224cc9a5590652e8700144d20
Michael Henretty <michael.henretty@gmail.com>
2016-08-08 09:07:15 -0600

    ping multiple times in serial and output statistics about it

--- client/js/main.js
+++ client/js/main.js
@@ -1,16 +1,40 @@
 function main() {
+  const PING_COUNT = 10;
+
   var id = Utility.guid();
   console.log('client id', id);
 
   var url = new URL(window.location.href);
   var peer = url.searchParams.get('peer');
 
+  function printPingResults(results) {
+    DOM.p('pinged results ' + results);
+    DOM.p('mean is ' + Utility.mean(results));
+    DOM.p('standard deviation is ' + Utility.stddev(results));
+  }
+
+  function pingSerial(count, cb) {
+    var results = [];
+    (function pingNext(curPing) {
+      if (curPing < count) {
+        Latency.ping(id, peer, (err, result) => {
+          if (err) {
+            console.log('ping error', err);
+          } else {
+            results.push(result);
+          }
+          pingNext(++curPing);
+        })
+      } else {
+        cb(null, results);
+      }
+    })(0);
+  }
+
   if (peer) {
     console.log('attempting to connect to peer', peer);
-    Latency.ping(id, peer, (err, result) => {
-      if (!err) {
-        DOM.p('ping result ' + result + 'ms');
-      }
+    pingSerial(PING_COUNT, (err, results) => {
+      printPingResults(results);
     });
   } else {
     console.log('waiting for connection');
--- client/js/utility.js
+++ client/js/utility.js
@@ -7,6 +7,22 @@ window.Utility = (function() {
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
       });
+    },
+
+    sum: function(data) {
+      return data.reduce((prev, cur) => prev + cur, 0);
+    },
+
+    mean: function(data) {
+      return Utility.sum(data) / data.length;
+    },
+
+    stddev: function(data) {
+      var mean = Utility.mean(data);
+      var sumOfDistances = Utility.sum(data.map(result => {
+        return Math.pow(mean - result, 2);
+      }));
+      return Math.sqrt(sumOfDistances / data.length);
     }
   };
 })();
